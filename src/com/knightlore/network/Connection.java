5228f3c0c9365374fd3781e97228005a61d15810
dxi695 <dxi695@cs.bham.ac.uk>
2018-01-20 18:29:01 +0000

    implementing connection over udp

--- src/com/knightlore/network/Connection.java
+++ src/com/knightlore/network/Connection.java
@@ -1,20 +1,91 @@
 package com.knightlore.network;
 
-import java.util.Date;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
 
-public class Connection {
-    private String uuid;
-    private Date lastComm;
+public class Connection extends Thread{
+	
+    private DatagramSocket socket;
+    private DatagramPacket packet;
+    private byte[] data;
+    private InetAddress address;
+    private long lastReceiving;
+    private final Object lock;
+    private Thread timeoutThread;
     
-    public Connection(String uuid) {
-        this.uuid = uuid;
+    public Connection(InetAddress address) {
+    	lastReceiving = System.currentTimeMillis();
+    	lock          = new Object();
+    	this.address  = address;
+        this.data     = new byte[256];
+        try {
+			this.socket = new DatagramSocket(Port.number);
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+        timeoutThread = new Thread() {
+        	
+        	private boolean runningAnnonymous = true;
+            
+        	public void run() {
+                while(runningAnnonymous){
+                	long currentTime = System.currentTimeMillis();
+                	if(currentTime - getLastReceivingTime() >= 5000){
+                		String message = "You have been disconnected.";
+                		send(message.getBytes());
+                		runningAnnonymous = false;
+                	}
+                }
+                
+            }
+
+        };
+        timeoutThread.start();
+    }
+    
+    private void setLastReceivingTime(long newReceivingTime){
+    	synchronized(lock){
+    		this.lastReceiving = newReceivingTime;
+    	}
+    }
+    
+    private long getLastReceivingTime(){
+    	synchronized(lock){
+    		return this.lastReceiving;
+    	}
+    }
+    
+    private boolean verify(byte[] data){
+    	//verify the ip from receciving packet
+    	return true;
     }
     
     public void send(byte[] data) {
-        
+    	try {
+    		this.packet = new DatagramPacket(data, data.length, this.address, Port.number);
+			socket.send(packet);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
     
     public byte[] receive() {
-        return null;
+       try {
+    	   this.packet = new DatagramPacket(data, data.length);
+    	   socket.receive(packet);
+    	   this.setLastReceivingTime(System.currentTimeMillis()); 
+    	   return packet.getData();
+       	} catch (IOException e) {
+       		e.printStackTrace();
+       	}
+       	return null;
     }
+    
+    public boolean isStillAlive(){
+    	return timeoutThread.isAlive();
+    }
+   
 }
--- src/com/knightlore/network/Manager.java
+++ src/com/knightlore/network/Manager.java
@@ -1,9 +1,14 @@
 package com.knightlore.network;
 
-import java.util.ArrayList;
+import java.util.Map;
 
 public class Manager {
-    private ArrayList<Connection> conns;
-    
+	//ip-connection map
+	private Map<String, Connection> connections;
+	
+	//infinite loop to listen for incoming loop
+	//here will be the verification of the convention for 4 bytes for example
+	//then create an entry in map
+	//and also start a new connection thread
     
 }
