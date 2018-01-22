a0fddd604a86a2250b67d5bb9a84597bde444905
Will Miller <willmiller97@gmail.com>
2018-01-22 12:53:36 +0000

    Reference Vector2D instead of Vector2

---
+++ src/com/knightlore/engine/Player.java
@@ -0,0 +1,28 @@
+package com.knightlore.engine;
+
+import com.knightlore.utils.Vector2D;
+
+public abstract class Player extends GameObject {
+
+    public Player(Vector2D position) {
+        super(position);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void onCreate() {
+    }
+
+    @Override
+    public void onUpdate() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onDestroy() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
