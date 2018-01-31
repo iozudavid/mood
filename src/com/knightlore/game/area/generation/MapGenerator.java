0761cc6133aa822b04b22aabae2a795c46733fd3
Thomas Williams <tjw625@student.bham.ac.uk>
2018-01-31 18:56:57 +0000

    a small start on Perlin Noise, finally understand what I need to do

--- src/com/knightlore/game/area/generation/MapGenerator.java
+++ src/com/knightlore/game/area/generation/MapGenerator.java
@@ -9,6 +9,7 @@ import com.knightlore.render.Environment;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
+import java.util.Vector;
 
 public class MapGenerator extends ProceduralGenerator {
     private double[][] perlinNoise;
@@ -42,8 +43,67 @@ public class MapGenerator extends ProceduralGenerator {
 
     private void createPerliNoiseForGrid(long seed) {
         // TODO implement
+    	// need a predictable way of getting same
+    	// pseudo-random vector for tile "corner"
+
     }
 
+    private double perlin(double x, double y){
+    	
+    	double xm = x % 1; // map to coordinates in unit square
+    	double ym = y % 1;
+    	
+    	double x0 = x - xm; // get coordinates of square points surrounding 
+    	double y0 = y - ym; // x any y
+    	double x1 = x0 + 1;
+    	double y1 = y0 + 1;
+    	
+    	//  (x0,y1)------(x1,y1)
+    	//      |           |
+    	//      |  (mx,ym)  |
+    	//      |           |
+    	//  (x0,y0)------(x1,y0)
+    	
+    	// get pseudo-random gradient vectors
+    	// should be predictable given x0, y0, x1, y1
+    	// 1
+    	// 2
+    	// 3
+    	// 4
+    	
+    	// generate distance vectors
+    	Vector<Double> d1,d2,d3,d4;
+    	// 1 (x0,y1) --> (x,y)
+    	d1 = new Vector<Double>(2);
+    	d1.add(0, x);
+    	d1.add(1, y - 1);
+    	// 2 (x1,y1) --> (x,y)
+    	d2 = new Vector<Double>(2);
+    	d2.add(0, x - 1);
+    	d2.add(1, y - 1);
+    	// 3 (x0,y0) --> (x,y)
+    	d3 = new Vector<Double>(2);
+    	d3.add(0, x);
+    	d3.add(0, y);
+    	// 4 (x1,y0) --> (x,y)
+    	d4 = new Vector<Double>(2);
+    	d4.add(0, x - 1);
+    	d4.add(0, y);
+    	
+    	// dot product gradient vectors with corresponding
+    	// distance vectors, generating the influence vectors
+    	// 1
+    	// 2
+    	// 3
+    	// 4
+    	
+    	// apply fade function (ease curve)
+    	
+    	// "interpolate" influence vectors
+    	
+    	return 0.0;
+    }
+    
     private void generateRooms(long seed) {
         // TODO implement
     }
