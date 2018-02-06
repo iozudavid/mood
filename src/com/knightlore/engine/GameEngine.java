7c88cf462e0bcf7919b4efa606fcaf2310ba344d
Will Miller <willmiller97@gmail.com>
2018-02-06 16:03:40 +0000

    spaces to tabs

--- src/com/knightlore/engine/GameEngine.java
+++ src/com/knightlore/engine/GameEngine.java
@@ -20,166 +20,166 @@ import com.knightlore.render.Screen;
  */
 public class GameEngine implements Runnable {
 
-    private static GameEngine singleton = null;
+	private static GameEngine singleton = null;
 
-    private static final double UPDATES_PER_SECOND = 60D;
-    public static final Ticker ticker = new Ticker();
+	private static final double UPDATES_PER_SECOND = 60D;
+	public static final Ticker ticker = new Ticker();
 
-    private final Screen screen;
-    private final MainWindow window;
+	private final Screen screen;
+	private final MainWindow window;
 
-    private World world;
+	private World world;
 
-    private final ArrayList<GameObject> objects;
-    private Thread thread;
-    private volatile boolean running = false;
+	private final ArrayList<GameObject> objects;
+	private Thread thread;
+	private volatile boolean running = false;
 
-    private LinkedList<GameObject> notifyToCreate;
-    private LinkedList<GameObject> notifyToDestroy;
+	private LinkedList<GameObject> notifyToCreate;
+	private LinkedList<GameObject> notifyToDestroy;
 
-    private boolean headless;
+	private boolean headless;
 
-    public GameEngine(boolean headless) {
-        this.headless = headless;
-        final int w = MainWindow.WIDTH, h = MainWindow.HEIGHT;
-        screen = new Screen(w, h);
-        window = new MainWindow(screen, MainWindow.TITLE, w, h);
+	public GameEngine(boolean headless) {
+		this.headless = headless;
+		final int w = MainWindow.WIDTH, h = MainWindow.HEIGHT;
+		screen = new Screen(w, h);
+		window = new MainWindow(screen, MainWindow.TITLE, w, h);
 
-        initInputs();
+		initInputs();
 
-        objects = new ArrayList<>();
-        notifyToCreate = new LinkedList<GameObject>();
-        notifyToDestroy = new LinkedList<GameObject>();
+		objects = new ArrayList<>();
+		notifyToCreate = new LinkedList<GameObject>();
+		notifyToDestroy = new LinkedList<GameObject>();
 
-        singleton = this;
-        world = new World(
-                AreaFactory.createRandomMap(Environment.LIGHT_OUTDOORS));
-    }
+		singleton = this;
+		world = new World(
+				AreaFactory.createRandomMap(Environment.LIGHT_OUTDOORS));
+	}
 
-    // FIXME: Refactor World and remove this
-    public World getWorld() {
-        return world;
+	// FIXME: Refactor World and remove this
+	public World getWorld() {
+		return world;
 
-    }
+	}
 
-    // FIXME: As above, regarding public modifier
-    public static GameEngine getSingleton() {
-        return singleton;
-    }
+	// FIXME: As above, regarding public modifier
+	public static GameEngine getSingleton() {
+		return singleton;
+	}
 
-    void addGameObject(GameObject g) {
-        // delay adding until next loop
-        notifyToCreate.add(g);
-    }
+	void addGameObject(GameObject g) {
+		// delay adding until next loop
+		notifyToCreate.add(g);
+	}
 
-    void removeGameObject(GameObject g) {
-        // delay deleting until next loop
-        notifyToDestroy.add(g);
-    }
+	void removeGameObject(GameObject g) {
+		// delay deleting until next loop
+		notifyToDestroy.add(g);
+	}
 
-    private void initInputs() {
-        System.out.println("Initialising Engine...");
-        InputManager.init();
-        setupKeyboard();
-        setupMouse();
-        System.out.println("Engine Initialised Successfully.");
-    }
+	private void initInputs() {
+		System.out.println("Initialising Engine...");
+		InputManager.init();
+		setupKeyboard();
+		setupMouse();
+		System.out.println("Engine Initialised Successfully.");
+	}
 
-    public void start() {
-        running = true;
-        thread = new Thread(this);
-        thread.start();
-        if (!headless)
-            window.setVisible(true);
-    }
+	public void start() {
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+		if (!headless)
+			window.setVisible(true);
+	}
 
-    public void stop() {
-        running = false;
-        try {
-            thread.join();
-        } catch (InterruptedException e) {
-            // should't happen as the thread shouldn't be interrupted
-            e.printStackTrace();
-        }
-    }
+	public void stop() {
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			// should't happen as the thread shouldn't be interrupted
+			e.printStackTrace();
+		}
+	}
 
-    @Override
-    public void run() {
-        /*
-         * This piece of code limits the number of game updates per second to
-         * whatever it is set to in the variable updatesPerSecond.
-         */
-        long lastTime = System.nanoTime();
-        double delta = 0D;
-        double ns = 1E9D / UPDATES_PER_SECOND;
-        while (running) {
-            long now = System.nanoTime();
-            delta += (now - lastTime) / ns;
-            lastTime = now;
+	@Override
+	public void run() {
+		/*
+		 * This piece of code limits the number of game updates per second to
+		 * whatever it is set to in the variable updatesPerSecond.
+		 */
+		long lastTime = System.nanoTime();
+		double delta = 0D;
+		double ns = 1E9D / UPDATES_PER_SECOND;
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
 
-            while (delta >= 1) {
-                updateObjects();
-                world.tick();
-                if (!headless)
-                    screen.render(0, 0, world);
-                delta -= 1;
+			while (delta >= 1) {
+				updateObjects();
+				world.tick();
+				if (!headless)
+					screen.render(0, 0, world);
+				delta -= 1;
 
-                ticker.tick();
-            }
-        }
-    }
+				ticker.tick();
+			}
+		}
+	}
 
-    private void updateObjects() {
-        // perform internal list management before updating.
-        // as modifying a list whilst iterating over it is a very bad idea.
+	private void updateObjects() {
+		// perform internal list management before updating.
+		// as modifying a list whilst iterating over it is a very bad idea.
 
-        Iterator<GameObject> it = notifyToCreate.iterator();
-        while (it.hasNext()) {
-            GameObject obj = it.next();
-            // add the object to the update list
-            objects.add(obj);
-            obj.setExists(true);
-            // notify the object it has been created
-            obj.onCreate();
-        }
-        notifyToCreate.clear();
+		Iterator<GameObject> it = notifyToCreate.iterator();
+		while (it.hasNext()) {
+			GameObject obj = it.next();
+			// add the object to the update list
+			objects.add(obj);
+			obj.setExists(true);
+			// notify the object it has been created
+			obj.onCreate();
+		}
+		notifyToCreate.clear();
 
-        // remove any objects that need deleting
-        it = notifyToDestroy.iterator();
-        while (it.hasNext()) {
-            GameObject obj = it.next();
-            // remove the object from the update list
-            objects.remove(obj);
-            obj.setExists(false);
-            // notify the object it has been effectively destroyed
-            obj.onDestroy();
-        }
-        notifyToDestroy.clear();
+		// remove any objects that need deleting
+		it = notifyToDestroy.iterator();
+		while (it.hasNext()) {
+			GameObject obj = it.next();
+			// remove the object from the update list
+			objects.remove(obj);
+			obj.setExists(false);
+			// notify the object it has been effectively destroyed
+			obj.onDestroy();
+		}
+		notifyToDestroy.clear();
 
-        // update all objects
-        for (GameObject obj : objects) {
-            obj.onUpdate();
-        }
+		// update all objects
+		for (GameObject obj : objects) {
+			obj.onUpdate();
+		}
 
-    }
+	}
 
-    /**
-     * Add the singleton keyboard instance to the canvas and request focus.
-     */
-    private void setupKeyboard() {
-        screen.addKeyListener(InputManager.getKeyboard());
-        screen.requestFocus();
-    }
+	/**
+	 * Add the singleton keyboard instance to the canvas and request focus.
+	 */
+	private void setupKeyboard() {
+		screen.addKeyListener(InputManager.getKeyboard());
+		screen.requestFocus();
+	}
 
-    /**
-     * Add the singleton mouse instance to the canvas and request focus.
-     */
-    private void setupMouse() {
-        Mouse mouse = InputManager.getMouse();
-        screen.addMouseListener(mouse);
-        screen.addMouseMotionListener(mouse);
-        screen.addMouseWheelListener(mouse);
-        screen.requestFocus();
-    }
+	/**
+	 * Add the singleton mouse instance to the canvas and request focus.
+	 */
+	private void setupMouse() {
+		Mouse mouse = InputManager.getMouse();
+		screen.addMouseListener(mouse);
+		screen.addMouseMotionListener(mouse);
+		screen.addMouseWheelListener(mouse);
+		screen.requestFocus();
+	}
 
 }
--- src/com/knightlore/game/Player.java
+++ src/com/knightlore/game/Player.java
@@ -19,148 +19,148 @@ import com.knightlore.utils.Vector2D;
 
 public class Player extends NetworkObject implements IRenderable {
 
-    private Camera camera;
-    private Weapon currentWeapon;
-    
-    private final Map<ServerControl, CameraGetterInterface> controlGettersMap;
-    private final Map<ServerControl, CameraSetterInterface> controlSettersMap;
+	private Camera camera;
+	private Weapon currentWeapon;
+	
+	private final Map<ServerControl, CameraGetterInterface> controlGettersMap;
+	private final Map<ServerControl, CameraSetterInterface> controlSettersMap;
 
-    public Player(UUID uuid, Camera camera) {
-        super(uuid);
-        this.camera = camera;
-        this.currentWeapon = new Shotgun();
-        
-        this.controlGettersMap = new HashMap<>();
-        this.controlGettersMap.put(ServerControl.XPOS, this.camera::getxPos);
-        this.controlGettersMap.put(ServerControl.YPOS, this.camera::getyPos);
-        this.controlGettersMap.put(ServerControl.XDIR, this.camera::getxDir);
-        this.controlGettersMap.put(ServerControl.YDIR, this.camera::getyDir);
-        this.controlGettersMap.put(ServerControl.XPLANE,
-                this.camera::getxPlane);
-        this.controlGettersMap.put(ServerControl.YPLANE,
-                this.camera::getyPlane);
+	public Player(UUID uuid, Camera camera) {
+		super(uuid);
+		this.camera = camera;
+		this.currentWeapon = new Shotgun();
+		
+		this.controlGettersMap = new HashMap<>();
+		this.controlGettersMap.put(ServerControl.XPOS, this.camera::getxPos);
+		this.controlGettersMap.put(ServerControl.YPOS, this.camera::getyPos);
+		this.controlGettersMap.put(ServerControl.XDIR, this.camera::getxDir);
+		this.controlGettersMap.put(ServerControl.YDIR, this.camera::getyDir);
+		this.controlGettersMap.put(ServerControl.XPLANE,
+				this.camera::getxPlane);
+		this.controlGettersMap.put(ServerControl.YPLANE,
+				this.camera::getyPlane);
 
-        this.controlSettersMap = new HashMap<>();
-        this.controlSettersMap.put(ServerControl.XPOS, this.camera::setxPos);
-        this.controlSettersMap.put(ServerControl.YPOS, this.camera::setyPos);
-        this.controlSettersMap.put(ServerControl.XDIR, this.camera::setxDir);
-        this.controlSettersMap.put(ServerControl.YDIR, this.camera::setyDir);
-        this.controlSettersMap.put(ServerControl.XPLANE,
-                this.camera::setxPlane);
-        this.controlSettersMap.put(ServerControl.YPLANE,
-                this.camera::setyPlane);
-        
-        
-    }
+		this.controlSettersMap = new HashMap<>();
+		this.controlSettersMap.put(ServerControl.XPOS, this.camera::setxPos);
+		this.controlSettersMap.put(ServerControl.YPOS, this.camera::setyPos);
+		this.controlSettersMap.put(ServerControl.XDIR, this.camera::setxDir);
+		this.controlSettersMap.put(ServerControl.YDIR, this.camera::setyDir);
+		this.controlSettersMap.put(ServerControl.XPLANE,
+				this.camera::setxPlane);
+		this.controlSettersMap.put(ServerControl.YPLANE,
+				this.camera::setyPlane);
+		
+		
+	}
 
-    public Vector2D getPosition() {
-        Vector2D pos = new Vector2D(camera.getxPos(), camera.getyPos());
-        return pos;
-    }
+	public Vector2D getPosition() {
+		Vector2D pos = new Vector2D(camera.getxPos(), camera.getyPos());
+		return pos;
+	}
 
-    public Vector2D getDirection() {
-        Vector2D dir = new Vector2D(camera.getxDir(), camera.getyDir());
-        return dir;
-    }
+	public Vector2D getDirection() {
+		Vector2D dir = new Vector2D(camera.getxDir(), camera.getyDir());
+		return dir;
+	}
 
-    public void setInputState(Map<ClientControl, Byte> inputState) {
-        camera.setInputState(inputState);
-    }
+	public void setInputState(Map<ClientControl, Byte> inputState) {
+		camera.setInputState(inputState);
+	}
 
-    @Override
-    public void render(PixelBuffer pix, int x, int y) {
-        pix.fillRect(0x000000, (int) this.position.getX(),
-                (int) this.position.getY(), 10, 50);
-    }
-
-
-    @Override
-    public void onUpdate() {
-    }
+	@Override
+	public void render(PixelBuffer pix, int x, int y) {
+		pix.fillRect(0x000000, (int) this.position.getX(),
+				(int) this.position.getY(), 10, 50);
+	}
 
 
-    @Override
-    public byte[] serialize(boolean disconnect) {
-        if (camera == null) {
-            System.out.println("Camera started null");
-            return null;
-        }
-        byte[] thisState = new byte[ServerProtocol.TOTAL_LENGTH];
+	@Override
+	public void onUpdate() {
+	}
 
-        // Prepend metadata to the state array.
-        byte[] metadata = ServerProtocol.getMetadata();
-        for (int i = 0; i < ServerProtocol.METADATA_LENGTH; i++) {
-            thisState[i] = metadata[i];
-        }
 
-        byte[] playerID = ServerProtocol.uuidAsBytes(super.objectUniqueID);
-        // add player id to the packet
-        for (int i = ServerProtocol.METADATA_LENGTH; i < ServerProtocol.METADATA_LENGTH
-                + ServerProtocol.OBJECTID_LENGTH; i++) {
-            thisState[i] = playerID[i - ServerProtocol.METADATA_LENGTH];
-        }
+	@Override
+	public byte[] serialize(boolean disconnect) {
+		if (camera == null) {
+			System.out.println("Camera started null");
+			return null;
+		}
+		byte[] thisState = new byte[ServerProtocol.TOTAL_LENGTH];
 
-        if(disconnect){
-            for (int i = ServerProtocol.MESSAGE_STARTING_POINT; i < ServerProtocol.TOTAL_LENGTH; i++) {
-                thisState[i] = ServerProtocol.disconnectedState[i - ServerProtocol.MESSAGE_STARTING_POINT];
-            }
-            //if disconnect then end it here with return statement
-            return thisState;
-        }
-        
-        // adding player stats from camera class
-        int loopsNumber = 0;
-        try {
-            for (int i = ServerProtocol.MESSAGE_STARTING_POINT; i < ServerProtocol.TOTAL_LENGTH;) {
-                // taking the current control
-                int positionCurrentCommand = loopsNumber;
-                ServerControl currentControl = ServerProtocol
-                        .getControlByPosition(positionCurrentCommand);
-                Integer[] indexes = ServerProtocol
-                        .getIndexesByControl(currentControl);
-                int startingIndex = indexes[0];
-                int endingIndex = indexes[1];
+		// Prepend metadata to the state array.
+		byte[] metadata = ServerProtocol.getMetadata();
+		for (int i = 0; i < ServerProtocol.METADATA_LENGTH; i++) {
+			thisState[i] = metadata[i];
+		}
 
-                CameraGetterInterface cameraReference = this.controlGettersMap
-                        .get(currentControl);
-                if (cameraReference == null)
-                    return null;
-                double cameraResult = cameraReference.accessDataFromCamera();
-                byte[] convertedCameraValue = ServerProtocol
-                        .doubleToByteArray(cameraResult);
+		byte[] playerID = ServerProtocol.uuidAsBytes(super.objectUniqueID);
+		// add player id to the packet
+		for (int i = ServerProtocol.METADATA_LENGTH; i < ServerProtocol.METADATA_LENGTH
+				+ ServerProtocol.OBJECTID_LENGTH; i++) {
+			thisState[i] = playerID[i - ServerProtocol.METADATA_LENGTH];
+		}
 
-                for (int j = startingIndex
-                        + ServerProtocol.MESSAGE_STARTING_POINT; j < endingIndex
-                                + ServerProtocol.MESSAGE_STARTING_POINT; j++) {
-                    thisState[j] = convertedCameraValue[j - startingIndex
-                            - ServerProtocol.MESSAGE_STARTING_POINT];
-                }
-                i += endingIndex - startingIndex;
-                loopsNumber++;
+		if(disconnect){
+			for (int i = ServerProtocol.MESSAGE_STARTING_POINT; i < ServerProtocol.TOTAL_LENGTH; i++) {
+				thisState[i] = ServerProtocol.disconnectedState[i - ServerProtocol.MESSAGE_STARTING_POINT];
+			}
+			//if disconnect then end it here with return statement
+			return thisState;
+		}
+		
+		// adding player stats from camera class
+		int loopsNumber = 0;
+		try {
+			for (int i = ServerProtocol.MESSAGE_STARTING_POINT; i < ServerProtocol.TOTAL_LENGTH;) {
+				// taking the current control
+				int positionCurrentCommand = loopsNumber;
+				ServerControl currentControl = ServerProtocol
+						.getControlByPosition(positionCurrentCommand);
+				Integer[] indexes = ServerProtocol
+						.getIndexesByControl(currentControl);
+				int startingIndex = indexes[0];
+				int endingIndex = indexes[1];
 
-            }
-        } catch (IOException e) {
-            System.err.println("Index not good...");
-            e.printStackTrace();
-        }
-        return thisState;
+				CameraGetterInterface cameraReference = this.controlGettersMap
+						.get(currentControl);
+				if (cameraReference == null)
+					return null;
+				double cameraResult = cameraReference.accessDataFromCamera();
+				byte[] convertedCameraValue = ServerProtocol
+						.doubleToByteArray(cameraResult);
 
-    }
+				for (int j = startingIndex
+						+ ServerProtocol.MESSAGE_STARTING_POINT; j < endingIndex
+								+ ServerProtocol.MESSAGE_STARTING_POINT; j++) {
+					thisState[j] = convertedCameraValue[j - startingIndex
+							- ServerProtocol.MESSAGE_STARTING_POINT];
+				}
+				i += endingIndex - startingIndex;
+				loopsNumber++;
 
-    @Override
-    public void deserialize(ServerCommand command) {
-        // TODO: only deserialise if the command's timestamp is recent enough.
-        Double val;
-        for (ServerControl c : ServerControl.values())
-            if ((val = command.getValueByControl(c)) != null)
-                this.controlSettersMap.get(c).setDataOnCamera(val);
+			}
+		} catch (IOException e) {
+			System.err.println("Index not good...");
+			e.printStackTrace();
+		}
+		return thisState;
 
-    }
+	}
 
-    @FunctionalInterface
-    public static interface CameraGetterInterface {
-        double accessDataFromCamera();
-    }
+	@Override
+	public void deserialize(ServerCommand command) {
+		// TODO: only deserialise if the command's timestamp is recent enough.
+		Double val;
+		for (ServerControl c : ServerControl.values())
+			if ((val = command.getValueByControl(c)) != null)
+				this.controlSettersMap.get(c).setDataOnCamera(val);
+
+	}
+
+	@FunctionalInterface
+	public static interface CameraGetterInterface {
+		double accessDataFromCamera();
+	}
 	public Camera getCamera() {
 		return camera;
 	}
@@ -169,19 +169,19 @@ public class Player extends NetworkObject implements IRenderable {
 		return currentWeapon;
 	}
 
-    @FunctionalInterface
-    public static interface CameraSetterInterface {
-        void setDataOnCamera(double val);
-    }
-    @Override
-    public void onCreate() {
-        // TODO Auto-generated method stub
-        
-    }
+	@FunctionalInterface
+	public static interface CameraSetterInterface {
+		void setDataOnCamera(double val);
+	}
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		
+	}
 
-    @Override
-    public void onDestroy() {
-        // TODO Auto-generated method stub
-        
-    }
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		
+	}
 }
