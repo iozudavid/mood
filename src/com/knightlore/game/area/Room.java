b98f1d9aea5ee91e68639ff6b69f13e42c22d71e
Kacper Kielak <kielak.kacper@gmail.com>
2018-02-08 13:42:57 +0000

    Add access to connections within a Room class

--- src/com/knightlore/game/area/Room.java
+++ src/com/knightlore/game/area/Room.java
@@ -6,6 +6,9 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Room extends Area {
+    public static final int MIN_CONNECTIONS = 2;
+    public static final int MAX_CONNECTIONS = 6;
+
     private int xPosition, yPosition;
     private final List<Room> connections = new LinkedList<>();
 
@@ -16,6 +19,19 @@ public class Room extends Area {
     public void setRoomPosition(int xPosition, int yPosition) {
         this.xPosition = xPosition;
         this.yPosition = yPosition;
+    }
+
+    public boolean addConnection(Room r) {
+        if (connections.size() < MAX_CONNECTIONS) {
+            connections.add(r);
+            return true;
+        }
+
+        return false;
+    }
+
+    public List<Room> getConnections() {
+        return connections;
     }
 
     public int getXPosition() {
