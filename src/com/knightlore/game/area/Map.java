9448f19ced48562a1ad446d6cb367542bbc1252b
Kacper Kielak <kielak.kacper@gmail.com>
2018-02-12 17:12:26 +0000

    Remove dead code from Map class

--- src/com/knightlore/game/area/Map.java
+++ src/com/knightlore/game/area/Map.java
@@ -1,12 +1,9 @@
 package com.knightlore.game.area;
-import com.knightlore.game.area.generation.MapGenerator;
 import com.knightlore.game.tile.Tile;
 import com.knightlore.render.Environment;
 
 public class Map extends Area {
-	
-    // TODO: read in maps from files/procedurally generate.
-    // Maps have associated environments
+    // TODO: read in maps from file
 	private final long seed;
 	private final Environment environment;
 
@@ -23,13 +20,4 @@ public class Map extends Area {
 	public long getSeed() {
 		return seed;
 	}
-
-	// dumb test
-	public static void main(String args[]) {
-		MapGenerator generator = new MapGenerator();
-		Map m = generator.createMap(100, 100, Environment.LIGHT_OUTDOORS);
-		// // m = joinUpR(m,m);
-		System.out.println(m.toString());
-	}
-
 }
