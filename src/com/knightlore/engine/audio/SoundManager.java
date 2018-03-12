47a2c182a0703ad350432f9a2186ae8d67923336
Will Miller <wjm656@student.bham.ac.uk>
2018-03-12 09:33:23 +0000

    Fix clip memory leak issue on some machines

--- src/com/knightlore/engine/audio/SoundManager.java
+++ src/com/knightlore/engine/audio/SoundManager.java
@@ -1,15 +1,26 @@
 package com.knightlore.engine.audio;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.LineUnavailableException;
 
-public class SoundManager {
+import com.knightlore.engine.GameObject;
+import com.knightlore.utils.pruner.Pruner;
+
+public class SoundManager extends GameObject {
     // 0-1, volume as a fraction.
     private final float DEFAULT_VOLUME;
-
+    //
+    private List<ClipWrapper> clips;
+    
     public SoundManager(float defaultVolume) {
         // The volume to play a clip at if not specified.
         this.DEFAULT_VOLUME = defaultVolume;
+        this.clips = Collections.synchronizedList(new ArrayList<>());
     }
 
     public SoundManager() {
@@ -18,30 +29,52 @@ public class SoundManager {
     }
 
     /**
-     * Play a one-time sound effect.
+     * Play a one-time sound resource.
      * 
      * @param e:
-     *            The sound effect to play.
+     *            The sound resource to play.
      * @param volume:
      *            The volume level to play at, as a float from 0 to 1.
      */
-    public void play(SoundResource e, float volume) {
-        Clip clip = e.getNewClip();
-        final FloatControl control = (FloatControl) clip
-                .getControl(FloatControl.Type.MASTER_GAIN);
+    public synchronized void play(SoundResource e, float volume) {
+        Clip clip;
+        try {
+            clip = e.getNewClip();
+        } catch (LineUnavailableException e1) {
+            // This can happen if, for example, we've reached the limit on
+            // number of open audio lines, on Linux systems. Silently fail,
+            // since audio effects aren't essential.
+            return;
+        }
+        final FloatControl control = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
         float range = control.getMaximum() - control.getMinimum();
         float gain = range * volume + control.getMinimum();
         control.setValue(gain);
         clip.start();
+        this.clips.add(new ClipWrapper(clip));
     }
 
     /**
-     * Play a one-time sound effect.
+     * Play a one-time sound resource.
      * 
      * @param e:
-     *            The sound effect to play.
+     *            The sound resource to play.
      */
     public void play(SoundResource e) {
         this.play(e, this.DEFAULT_VOLUME);
+    }
+
+    @Override
+    public void onUpdate() {
+        // Ensure all finished clips are cleared up.
+        Pruner.prune(clips);
+    }
+
+    @Override
+    public void onDestroy() {
+    }
+
+    @Override
+    public void onCreate() {
     }
 }
