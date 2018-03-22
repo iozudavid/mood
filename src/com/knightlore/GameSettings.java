922f1d42565c58cb3c6ff0c0ac87a4ca2e53c018
Joe Ellis <joechrisellis@gmail.com>
2018-03-22 16:25:02 +0000

    tried to fix a bug.

--- src/com/knightlore/GameSettings.java
+++ src/com/knightlore/GameSettings.java
@@ -8,7 +8,8 @@ public final class GameSettings {
     static boolean client = false;
     static boolean server = false;
 
-    public static int desiredBlockiness = 10;
+    public static final int DEFAULT_BLOCKINESS = 10;
+    public static int desiredBlockiness = DEFAULT_BLOCKINESS;
     public static int actualBlockiness = desiredBlockiness;
 
     // FIXME: enter dynamic player names in the GUI.
