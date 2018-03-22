53c341de3ee8e3bb23ebb184793958caa2234b56
Kacper Kielak <kielak.kacper@gmail.com>
2018-03-22 15:35:37 +0000

    Add all possible tiles to the LevelEditor

--- src/com/knightlore/leveleditor/TileChooserPanel.java
+++ src/com/knightlore/leveleditor/TileChooserPanel.java
@@ -1,6 +1,8 @@
 package com.knightlore.leveleditor;
 
 import com.google.common.collect.ImmutableList;
+import com.knightlore.game.Team;
+import com.knightlore.game.entity.pickup.PickupType;
 import com.knightlore.game.tile.*;
 
 import javax.swing.Box;
@@ -11,7 +13,16 @@ public class TileChooserPanel extends JPanel {
     private static final ImmutableList<Tile> OPTIONS = ImmutableList.of(
             AirTile.getInstance(),
             new BrickTile(),
-            new BushTile()
+            new BushTile(),
+            new LavaTile(),
+            new MossBrickTile(),
+            new PickupTile(PickupType.SHOTGUN),
+            new PickupTile(PickupType.HEALTH),
+            new PlayerSpawnTile(Team.BLUE),
+            new PlayerSpawnTile(Team.RED),
+            new TurretTile(Team.BLUE),
+            new TurretTile(Team.RED),
+            new TurretTile(Team.NONE)
     );
 
     public TileChooserPanel() {
