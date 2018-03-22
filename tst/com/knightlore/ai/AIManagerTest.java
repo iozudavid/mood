fb7c7bc7c83e26946c50d5ebe74f2c362a8d0ad0
Kacper Kielak <kielak.kacper@gmail.com>
2018-03-22 12:54:45 +0000

    Add path pruning tests to AIManagerTest

--- tst/com/knightlore/ai/AIManagerTest.java
+++ tst/com/knightlore/ai/AIManagerTest.java
@@ -2,24 +2,35 @@ package com.knightlore.ai;
 
 import com.knightlore.game.area.Map;
 import com.knightlore.utils.Vector2D;
+import com.knightlore.utils.physics.Physics;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.awt.Point;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.doReturn;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Physics.class)
 public class AIManagerTest {
 
     @Test
     public void findRawPath() {
         // Given
         Map map = mock(Map.class);
-        when(map.getCostGrid()).thenReturn(new double[][]{
+        when(map.getCostGrid()).thenReturn(new double[][] {
                 {5, 5, 1, 1, 1, 5, 5},
                 {5, 1, 1, 5, 1, 1, 5},
                 {1, 1, 5, 5, 5, 1, 5},
@@ -43,8 +54,60 @@ public class AIManagerTest {
     }
 
     @Test
-    public void pruneUnnecessaryNodes() {
-        // TODO power mock
+    public void pruneUnnecessaryNodes_prunablePath() throws Exception {
+        // Given
+        Vector2D start = new Vector2D(3, 3);
+        Vector2D end = new Vector2D(5, 8);
+        Vector2D visibleFromStart = new Vector2D(5, 7);
+        List<Point> rawPath = new LinkedList<>(Arrays.asList(
+                start.toPoint(), new Point(3, 4), new Point(3, 5), new Point(4, 5),
+                new Point(4, 6), new Point(4, 7), visibleFromStart.toPoint(), end.toPoint()
+        ));
 
+        // When
+        Map map = mock(Map.class);
+        when(map.getCostGrid()).thenReturn(new double[][]{{0}});
+
+        mockStatic(Physics.class);
+        when(Physics.linecastQuick(any(Vector2D.class), any(Vector2D.class), eq(AIManager.PATH_SMOOTH_ACCURACY)))
+                .thenThrow(new IllegalStateException("This method shouldn't be called, AIManager should alread return"));
+        doReturn(true).when(Physics.class, "linecastQuick",
+                eq(start), eq(end), eq(AIManager.PATH_SMOOTH_ACCURACY));
+        doReturn(false).when(Physics.class, "linecastQuick",
+                eq(start), eq(visibleFromStart), eq(AIManager.PATH_SMOOTH_ACCURACY));
+
+        AIManager aiManager = new AIManager(map);
+
+        List<Point> prunedPath = aiManager.pruneUnnecessaryNodes(new LinkedList<>(rawPath));
+
+        // Then
+        List<Point> expectedPath = Arrays.asList( visibleFromStart.toPoint(), end.toPoint());
+        assertThat(prunedPath, is(expectedPath));
+    }
+
+    @Test
+    public void pruneUnnecessaryNodes_unprunablePath() {
+        // Given
+        Vector2D start = new Vector2D(3, 3);
+        Vector2D end = new Vector2D(5, 8);
+        List<Point> rawPath = new LinkedList<>(Arrays.asList(
+                start.toPoint(), new Point(3, 4), new Point(3, 5), new Point(4, 5),
+                new Point(4, 6), new Point(4, 7), new Point(5, 7), end.toPoint()
+        ));
+
+        // When
+        Map map = mock(Map.class);
+        when(map.getCostGrid()).thenReturn(new double[][]{{0}});
+
+        mockStatic(Physics.class);
+        when(Physics.linecastQuick(any(Vector2D.class), any(Vector2D.class), eq(AIManager.PATH_SMOOTH_ACCURACY)))
+                .thenReturn(true);
+
+        AIManager aiManager = new AIManager(map);
+
+        List<Point> prunedPath = aiManager.pruneUnnecessaryNodes(new LinkedList<>(rawPath));
+        
+        // Then
+        assertThat(prunedPath, is(rawPath));
     }
 }
