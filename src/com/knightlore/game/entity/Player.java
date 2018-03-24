a34aa84e1dbdd00d732a31f173d3be11bab77d75
James Adey <jxa627@cs.bham.ac.uk>
2018-03-24 18:02:58 +0000

    doc build methods

--- src/com/knightlore/game/entity/Player.java
+++ src/com/knightlore/game/entity/Player.java
@@ -87,7 +87,18 @@ public class Player extends Entity {
 
     private PlayerGraphicMatrix.Color color = PlayerGraphicMatrix.Color.RED;
 
-    // Returns a new instance. See NetworkObject for details.
+    /**
+     * Called by the network when creating the client-side representation of
+     * this object. Instantiates a copy of the client class, and deserializes
+     * the state into it.
+     * 
+     * @param uuid
+     *            The uuid provided to this object
+     * @param state
+     *            The initial state of this object
+     * @returns The client-side network object
+     * @see NetworkObject
+     */
     public static NetworkObject build(UUID uuid, ByteBuffer state) {
         System.out.println("Player build, state size: " + state.remaining());
         NetworkObject obj = new Player(uuid, Vector2D.ONE, Vector2D.ONE);
