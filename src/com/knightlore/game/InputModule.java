04891993f13e20d872b8fc59dfaf54e989f8fa52
James Adey <jxa627@cs.bham.ac.uk>
2018-03-24 16:50:33 +0000

    input module docs

--- src/com/knightlore/game/InputModule.java
+++ src/com/knightlore/game/InputModule.java
@@ -5,8 +5,14 @@ import java.util.Map;
 import com.knightlore.game.entity.Entity;
 import com.knightlore.network.protocol.ClientController;
 
+/**
+ * The input component of a player, with human and bot implementations.
+ * @authors James Adey, Tom Williams
+ *
+ */
 public abstract class InputModule {
     /*
+     * Instructions that can be placed in a map
      * FORWARD, LEFT, BACKWARD, RIGHT, ROTATE_CLOCKWISE, ROTATE_ANTI_CLOCKWISE,
      * SHOOT
      */
@@ -15,7 +21,17 @@ public abstract class InputModule {
 	public double walkInput;
 	public boolean shoot;
 	
+	/**
+	 * Updates the input state provided to it
+	 * @param inputState the initial input state of the player
+	 * @param myPlayer the player that this input module is controlling
+	 * @returns a modified input mapping for the player 
+	 */
 	public abstract Map<ClientController, Byte> updateInput(Map<ClientController, Byte> inputState, Entity myPlayer);
 
+	/**
+	 * Used to notify the input that the player it is controlling has respawned
+	 * @param myPlayer The player which this input module is controlling
+	 */
     public abstract void onRespawn(Entity myPlayer);
 }
