4fe723339c950f55ebbe6fd0bc9b61c548dae504
James Adey <jxa627@cs.bham.ac.uk>
2018-03-24 18:51:02 +0000

    documented managers

--- src/com/knightlore/game/manager/ClientFFAGameManager.java
+++ src/com/knightlore/game/manager/ClientFFAGameManager.java
@@ -7,33 +7,63 @@ import com.knightlore.GameSettings;
 import com.knightlore.engine.GameEngine;
 import com.knightlore.network.NetworkObject;
 
+/**
+ * Contains data and methods to handle the client-side of the Free For All game
+ * mode
+ * 
+ * @author James
+ *
+ */
 public class ClientFFAGameManager extends FFAGameManager {
-
+    
+    /**
+     * Creates a Game Manager with a random UUID.
+     */
     public ClientFFAGameManager() {
         super(UUID.randomUUID());
     }
-
+    
+    /**
+     * Creates a Game Manager with the given UUID.
+     * 
+     * @param uuid
+     *            the UUID of this network object
+     */
     public ClientFFAGameManager(UUID uuid) {
         super(uuid);
         GameEngine.getSingleton().getWorld().changeGameManager(this);
     }
-
-    // Returns a new instance. See NetworkObject for details.
+    
+    /**
+     * Called by the network when creating the client-side representation of
+     * this object. Instantiates a copy of the client class, and deserializes
+     * the state into it.
+     * 
+     * @param uuid
+     *            The uuid provided to this object
+     * @param state
+     *            The initial state of this object
+     * @returns The client-side network object
+     * @see NetworkObject
+     */
     public static NetworkObject build(UUID uuid, ByteBuffer state) {
-        System.out.println("Player build, state size: " + state.remaining());
+        System.out.println("FFA CLIENT build, state size: " + state.remaining());
         NetworkObject obj = new ClientFFAGameManager(uuid);
         obj.init();
         obj.deserialize(state);
         return obj;
     }
-
+    
     @Override
     public String getClientClassName() {
         return ClientFFAGameManager.class.getName();
     }
-
+    
     boolean gameOver = false;
-
+    
+    /**
+     * Checks if the game is over.
+     */
     @Override
     public void onUpdate() {
         if (!gameOver && gameState == GameState.FINISHED) {
@@ -41,10 +71,10 @@ public class ClientFFAGameManager extends FFAGameManager {
             gameOver = true;
         }
     }
-
+    
     private void onGameOver() {
         GameSettings.desiredBlockiness = 70;
         System.out.println("!!!!!");
     }
-
+    
 }
