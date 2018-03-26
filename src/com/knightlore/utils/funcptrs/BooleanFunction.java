e51074096276e94911a46a7b8e58db945f43bf01
James Adey <jxa627@cs.bham.ac.uk>
2018-03-26 11:12:28 +0100

    documented more helper classes, cleaned up codebase

--- src/com/knightlore/utils/funcptrs/BooleanFunction.java
+++ src/com/knightlore/utils/funcptrs/BooleanFunction.java
@@ -1,5 +1,13 @@
 package com.knightlore.utils.funcptrs;
 
+/**
+ * A functional interface, that takes some parameter and returns a boolean, used
+ * in the GUI.
+ * 
+ * @author James
+ *
+ * @param <T>
+ */
 @FunctionalInterface
 public interface BooleanFunction<T> {
     boolean check(T value);
--- src/com/knightlore/utils/funcptrs/VoidFunction.java
+++ src/com/knightlore/utils/funcptrs/VoidFunction.java
@@ -1,5 +1,13 @@
 package com.knightlore.utils.funcptrs;
 
+/**
+ * A functional interface, used in the GUI as a function pointer to a void
+ * method.
+ * 
+ * @author James
+ *
+ * @param <T>
+ */
 @FunctionalInterface
 public interface VoidFunction {
     void call();
